cc2ecc6ec4e00370f42d3300ea0b3258d7dccbd6
Sophie <sophieolushola02@gmail.com>
2025-01-15 13:45:34 +0100

    first puppet code

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+using puppet to create a file in /tmp
+
+file { 'new_project':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
