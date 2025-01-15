cac5907ec74099476b5e6d9a9d9434378d748af4
Sophie <sophieolushola02@gmail.com>
2025-01-15 14:22:41 +0100

    killing a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #creating a manifest that kills a process names killmenow
 
 exec{'killmenow':
-  command  => 'pkill -f killmenow',
+  command => '/usr/bin/pkill -f killmenow',
+  path    => '/usr/local/bin/' ,'/bin/',
 }
